3118eade814e18eb18e32df082ae64780ecdafef
Wzicheng <592817373@qq.com>
2017-10-24 11:24:08 +0800

    原基础上更新我页面

--- app/src/main/java/com/bester/chat/xim/controller/fragment/MineFragment.java
+++ app/src/main/java/com/bester/chat/xim/controller/fragment/MineFragment.java
@@ -1,12 +1,19 @@
 package com.bester.chat.xim.controller.fragment;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.view.Gravity;
+import android.content.Intent;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.bester.chat.xim.R;
+import com.bester.chat.xim.controller.activity.SplashActivity;
+import com.bester.chat.xim.model.Model;
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
+
+import static android.R.attr.starStyle;
+import static android.R.attr.start;
 
 /**
  * Created by Wzich on 2017/10/23.
@@ -14,13 +21,15 @@ import com.bester.chat.xim.R;
 
 public class MineFragment extends BaseFragment {
 
+    private Button mBtnMineOut;
+
     public MineFragment(Context context) {
         super(context);
     }
 
     @Override
     public View initView() {
-        View view = View.inflate(context, R.layout.activity_mine,null);
+        View view = View.inflate(context, R.layout.activity_mine, null);
         return view;
     }
 }
